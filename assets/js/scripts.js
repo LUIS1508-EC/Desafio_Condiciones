7ff109d09674845f68bba9d19bbe425c220a0360
LUIS1508-EC <151102084+LUIS1508-EC@users.noreply.github.com>
2024-03-08 00:20:21 -0300

    Desarrollo

---
+++ assets/js/scripts.js
@@ -0,0 +1,12 @@
+var activo = false
+
+var parrafo = document.querySelector("p")
+var contraseña = document.querySelector(".primero").value
+
+if(contraseña == 1){
+    alert('adelante')
+}else if(contraseña == 2){
+    alert('para un poco')
+}else{
+    alert('no pases')
+}
